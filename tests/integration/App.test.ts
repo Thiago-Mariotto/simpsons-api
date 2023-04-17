8ada965e86550cbb064b1920998c07ea954922ac
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-04-17 19:45:58 -0300

    test: add integration tests

---
+++ tests/integration/App.test.ts
@@ -0,0 +1,21 @@
+import 'jest';
+import request from 'supertest';
+import app from '../../src/App';
+import connection from '../../src/database/Connection';
+import { executeQueries, readQueries } from '../../src/database/queryUtils';
+
+describe('App', () => {
+  it('deve retornar 404 ao acessar uma pagina invalida', async () => {
+    const result = await request(app).get('/invalid');
+    expect(result.status).toEqual(404);
+  });
+  it('deve retornar 500 caso nao tenha database', async () => {
+    await executeQueries(connection, readQueries('dropDatabase.sql'));
+    await connection.end();
+    console.error = jest.fn();
+
+    const result = await request(app).get('/casts');
+    expect(result.status).toEqual(500);
+    expect(console.error).toHaveBeenCalled();
+  });
+});
